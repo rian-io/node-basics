20891de5795541d161b10df3da3aa45a8fcca756
Rian I. de Oliveira <rian-io@users.noreply.github.com>
2019-07-28 21:05:01 -0300

    Sixth lesson
    Async await.

--- Routes/users.js
+++ Routes/users.js
@@ -3,47 +3,49 @@ const router = express.Router();
 const Users = require('../model/user');
 const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-  Users.find({}, (err, data) => {
-    if (err) return res.send({ error: 'Erro na consulta de usuários!' });
-    return res.send(data);
-  });
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.find({});
+    return res.send(users);
+  }
+  catch (err) {
+    return res.send({ error: 'Erro na consulta de usuários!' });
+  }
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   const { email, password } = req.body;
 
   if(!email || !password) return res.send({ error: 'Dados insuficientes!' });
 
-  Users.findOne({ email }, (err, data) => {
-    if (err) return res.send({ error: 'Erro ao buscar usuário!' });
-    if (data) return res.send({ error: 'Usuário já registrado!' });
+  try {
+    if (await Users.findOne({ email })) return res.send({ error: 'Usuário já registrado!' });
 
-    Users.create(req.body, (err, data) => {
-      if (err) return res.send({ error: 'Erro ao criar usuário!' });
-
-      data.password = undefined;
-      return res.send(data);
-    });
-  });
+    const user = await Users.create(req.body)    ;
+    user.password = undefined;
+    return res.send(user);
+  } catch (err) {
+    return res.send({ error: 'Erro ao buscar usuário!' });
+  }
 });
 
-router.post('/auth', (req, res) => {
+router.post('/auth', async (req, res) => {
   const { email, password } = req.body;
 
   if(!email || !password) return res.send({ error: 'Dados insuficientes!' });
 
-  Users.findOne({ email }, (err, data) => {
-    if (err) return res.send({ error: 'Erro ao buscar usuário!' });
-    if (!data) return res.send({ error: 'Usuário não registrado!' });
+  try {
+    const user = await Users.findOne({ email }).select('+password');
+    if (!user) return res.send({ error: 'Usuário não registrado!' });
 
-    bcrypt.compare(password, data.password, (err, same) => {
-      if (!same) return res.send({ error: 'Erro ao autenticar usuário!' });
+    const pass_ok = await bcrypt.compare(password, user.password);
+    if (!pass_ok) return res.send({ error: 'Erro ao autenticar usuário!' });
 
-      data.password = undefined;
-      return res.send(data);
-    });
-  }).select('+password');
+    user.password = undefined;
+    return res.send(user);
+  } catch (err) {
+    return res.send({ error: 'Erro ao buscar usuário!' });
+  }
 });
 
 module.exports = router;
